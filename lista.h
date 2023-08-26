fa53bdfedf20b490d065996c1faddbe47b9002b6
Luis <lpoveda@estudiantec.cr>
2023-08-25 20:53:51 -0600

    added the files to the repository

---
+++ lista.h
@@ -0,0 +1,40 @@
+/*
+Hecho por Luis Poveda Gutiérrez
+c.2022085302
+
+Tipo de lista a usar:
+Se utilizará una lista doblemente enlazada, ya que estas permiten desplazarse de forma más fácil entre los nodos del array,
+así como por la facilidad que representan estas para insertar y eliminar elementos.
+*/
+
+//Métodos a utilizar:
+
+//insertData (string data){}
+// Este se utiliza para insertar noticias al inicio de la lista.
+
+//insertIntoPosition(string data, int position){}
+//Este inserta noticias en la posición que se le indica.
+
+//searchString(string keyword){}
+//Este busca entre los títulos de las noticias en el array, la keyword dada.
+
+//searchPosition(int position){}
+//Este busca la data incluída en la posición dada.
+
+//searchRelevance(int relevance){}
+//Este busca las noticias por relevancia.
+
+//deleteData(string data){}
+//Este elimina la data al inicio del array.
+
+//deleteIntoPosition(string data, int position){}
+//Este elimina la data en la posición dada.
+
+//displayNewsInPosition(int position){}
+//Este muestra las noticias que hay en la posición dada.
+
+//nextTitle(int memory){}
+//Este muestra la siguiente noticia a la que se acaba de solicitar.
+
+//reorderTitle(int position, int newPosition){}
+//Este toma la posición del titular dado y lo mueve a la nueva posición.
---
+++ metodos.h
@@ -0,0 +1,35 @@
+/*
+Hecho por Luis Poveda Gutiérrez
+c.2022085302
+
+Tipo de lista a usar:
+Se utilizará una lista doblemente enlazada, ya que estas permiten desplazarse de forma más fácil entre los nodos del array,
+así como por la facilidad que representan estas para insertar y eliminar elementos.
+*/
+
+//Métodos a utilizar:
+
+//fiveMostRelevant(){
+//displayNewsInPosition();
+//nextTitle();}
+//Este muestra los 5 titulares más relevantes.
+
+//displayByRelevance(){
+//displayNewsInPosition();
+//nextTitle();}
+//Este muestra las noticias por relevancia.
+
+//searchByFilter(){
+//searchString();
+//nextTitle();}
+//Este muestra las noticias que se buscan en el string.
+
+//deleteByFilter(){
+//searchString();
+//deleteData();}
+//Este elimina las noticias que se buscan con el filtro.
+
+//changeRelevance(){
+// searchRelevance();
+// reorderTitle();}
+//Este cambia y reordena según la relevancia.
